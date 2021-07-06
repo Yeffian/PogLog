608b282d6745a006cf922ae3b447bb97071a5235
Yeff <76024824+YeffyCodeGit@users.noreply.github.com>
2021-07-06 14:49:05 +0600

    a

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import io.github.yeffycodegit.poglog.PogLogger;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final PogLogger LOGGER = new PogLogger(true);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println(">> ");
+            String o = scanner.nextLine();
+            if (o.equals("stop")) {
+                Exception e = new IllegalStateException("idfk");
+                LOGGER.error(e);
+                break;
+            } else {
+                LOGGER.info(o);
+            }
+        }
+    }
+}
