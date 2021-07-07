94f9624e78e41c541fc15f8b4f8b275b728a4d6d
AurumByte <80045511+AurumByte@users.noreply.github.com>
2021-07-07 13:58:21 +0530

    refactor some changes

---
+++ src/main/java/io/github/yeffycodegit/poglog/PogLogger.java
@@ -0,0 +1,73 @@
+package io.github.yeffycodegit.poglog;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.io.github.yeffycodegit.poglog.ColorConstants;
+
+public class PogLogger {
+    private final boolean logToFile;
+    private String filename;
+    FileWriter fileWriter;
+
+    public PogLogger(boolean logToFile) throws IOException {
+        this.logToFile = logToFile;
+        this.filename = "app-logs.txt";
+
+        fileWriter = new FileWriter(filename);
+    }
+
+    public void success(Object msg) throws IOException {
+        if (logToFile) {
+            fileWriter.write("[SUCCESS] " + msg.toString() + "\n");
+            fileWriter.flush();
+            return;
+        }
+
+        System.out.printf(GREEN + "[SUCCESS] %s%n" + RESET, msg.toString());
+    }
+
+    public void info(Object msg) throws IOException {
+        if (logToFile) {
+            fileWriter.write("[INFO] " + msg.toString() + "\n");
+            fileWriter.flush();
+            return;
+        }
+
+        System.out.printf(BLUE + "[INFO] %s%n" + RESET, msg.toString());
+    }
+
+    public void warn(Object msg) throws IOException {
+        if (logToFile) {
+            fileWriter.write("[WARN] " + msg.toString() + "\n");
+            fileWriter.flush();
+            return;
+        }
+
+        System.out.printf(YELLOW + "[WARN] %s%n" + RESET, msg.toString());
+    }
+
+    public void error(Object msg) throws IOException {
+        if (logToFile) {
+            fileWriter.write("[ERROR] " + msg.toString() + "\n");
+            fileWriter.flush();
+            return;
+        }
+
+        System.out.printf(RED + "[ERROR] %s%n" + RESET, msg.toString());
+    }
+
+    public void error(Exception e) throws IOException {
+        if (logToFile) {
+            fileWriter.write("[ERROR] " + e.toString() + "\n");
+            fileWriter.write("     [STACK TRACE] " + Arrays.toString(e.getStackTrace()) + "\n");
+            fileWriter.write("     [CAUSE] " + e.getCause() + "\n");
+            fileWriter.flush();
+            return;
+        }
+
+        System.out.printf(RED + "[ERROR] %s%n" + RESET, e.toString());
+        System.out.printf(RED + "    [STACK TRACE] %s%n" + RESET, Arrays.toString(e.getStackTrace()));
+        System.out.printf(CYAN + "    [CAUSE] %s%n" + RESET, e.getCause());
+    }
+}
